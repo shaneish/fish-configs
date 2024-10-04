7dbf5ef4beab3d22e7a6cf9ccf4c35f2cd8d1d06
H62756 <h62756@JJ4DNV2JYM.tld>
2024-10-04 10:48:02 -0400

    ¯＼(º_o)/¯

--- locals/default.local.fish
+++ locals/default.local.fish
@@ -3,3 +3,4 @@ alias ipy="python3 -m ipython"
 
 set BROWSER "/usr/bin/google-chrome-stable"
 set EDITOR "/usr/bin/nvim"
+set SHELL "/usr/bin/fish"
--- locals/h62756.local.fish
+++ locals/h62756.local.fish
@@ -4,6 +4,9 @@ alias ipy="$PY_VENV_BIN/ipython"
 
 source "$FISH_CONFIG_DIR/locals/cigna_certs.fish"
 
+set EDITOR "/opt/homebrew/bin/nvim"
+set SHELL "/opt/homebrew/bin/fish"
+
 function dbsrc --description "Search the Databricks Python SDK codebase for a string"
     set -l dir (hp f dbrx-sdk)
     hp dbrx-sdk
