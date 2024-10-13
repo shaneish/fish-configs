3abeb5ab445f2732e9dcaf1e0a8033e902604205
H62756 <h62756@JJ4DNV2JYM.tld>
2024-10-13 18:59:09 -0400

    ...

--- functions.fish
+++ functions.fish
@@ -108,3 +108,12 @@ function fish_user_key_bindings
     bind ! bind_bang
     bind '$' bind_dollar
 end
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
