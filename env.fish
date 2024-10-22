474af7bae2ce689d664040807f5812cc804b3b64
Shane Stephenson <stephenson.shane.a@gmail.com>
2024-10-22 03:48:13 -0400

    ヽ(´▽`)/

--- env.fish
+++ env.fish
@@ -52,7 +52,7 @@ if type -q "bhop"
     alias _hp_fzf_fixed="bhop __bhop_list__ | fnk filter -f 'f -> \":\" not in f' | fzf -m | fnk map -f 'f -> f.split()[-1]' | xargs"
     alias _hp_fzf="bhop __bhop_list__ | rg '\->' | fzf -m | awk -F'->' '{print $2}' | xargs"
     alias hg="cd (_hp_fzf_fixed)"
-    alias hf="_hp_fzf_fixed $EDITOR"
+    alias ho="_hp_fzf_fixed $EDITOR"
 end
 
 set -gx CONFIG_DIRECTORY $HOME/.config
